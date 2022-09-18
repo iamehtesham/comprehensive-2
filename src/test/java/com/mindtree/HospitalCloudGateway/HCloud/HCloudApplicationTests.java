ef6e1bbee0b0d8f3fd47c869b073f86a1f91a584
unknown <mindc1july193@iiht.tech>
2022-09-18 21:26:49 +0530

    Initial commit

---
+++ src/test/java/com/mindtree/HospitalCloudGateway/HCloud/HCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.HospitalCloudGateway.HCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
